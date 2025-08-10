d39e374dd5ea1be204db405fed125f540e6c50d9
joao0814 <joaopmfranca@gmail.com>
2025-08-10 11:59:04 -0300

    feat: creat navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.scss';
+import {ReactComponent as Logo} from '../../assets/logo.svg';
+
+export default function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <Logo className={styles.logo}/>
+      <h1>Navbar</h1>
+    </nav>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Opa</div>
+    <Navbar />
   </React.StrictMode>
 );
 
