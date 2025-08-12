96389d8b48de6d34b13a4b8915dfc5a7e0d5f352
joao0814 <joaopmfranca@gmail.com>
2025-08-12 19:46:53 -0300

    feat: incrementing nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,42 @@
-import styles from './Navbar.module.scss';
-import {ReactComponent as Logo} from '../../assets/logo.svg';
+import styles from "./Navbar.module.scss";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import classNames from "classnames";
+import { useLocation } from "react-router-dom";
+import { RiShoppingCart2Line, RiShoppingCartFill } from "react-icons/ri";
 
-export default function Navbar({children}) {
+export default function Navbar({ children }) {
+  const location = useLocation();
+
+  const iconeProps = {
+    color: "white",
+    size: "24px",
+  };
+
   return (
     <nav className={styles.nav}>
-      <Logo className={styles.logo}/>
+      <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <div>
+          <a
+            href="/"
+            className={classNames(styles.link, {
+              [styles.selected]: location.pathname === "/",
+            })}
+          >
+            Home
+          </a>
+        </div>
+      </div>
+      <div className={styles.busca}>opa</div>
+      <div className={styles.icone}>
+        <a href="/carrinho">
+          {location.pathname === "/carrinho" ? (
+            <RiShoppingCartFill {...iconeProps} />
+          ) : (
+            <RiShoppingCart2Line {...iconeProps} />
+          )}
+        </a>
+      </div>
       <div>{children}</div>
     </nav>
   );
