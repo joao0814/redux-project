31096191bad54031281a243bad5cc379d1de398b
joao0814 <joaopmfranca@gmail.com>
2025-08-16 19:52:15 -0300

    cchore: change language

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,29 @@
 import style from "./Footer.module.scss";
 import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
-const iconeProps = {
+const iconProps = {
   color: "white",
   size: 24,
+  "aria-label": "Social media icon",
 };
 
 export default function Footer() {
   return (
     <footer className={style.footer}>
       <div>
-        <FaFacebook className={style.icon} {...iconeProps} />
-        <FaTwitter className={style.icon} {...iconeProps} />
-        <FaInstagram className={style.icon} {...iconeProps} />
+        <FaFacebook
+          className={style.icon}
+          {...iconProps}
+          aria-label="Facebook"
+        />
+        <FaTwitter className={style.icon} {...iconProps} aria-label="Twitter" />
+        <FaInstagram
+          className={style.icon}
+          {...iconProps}
+          aria-label="Instagram"
+        />
       </div>
-      <span>Desenvolvido por João</span>
+      <span>Developed by João</span>
     </footer>
   );
 }
