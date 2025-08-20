a231b7c6a0853d833817691ea7d81f8e808631cd
joao0814 <joaopmfranca@gmail.com>
2025-08-20 16:22:05 -0300

    feat: integrated redux in project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import Rotas from 'routes';
-import { initWebVitals } from './utils/webVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Rotas from "routes";
+import { initWebVitals } from "./utils/webVitals";
+import { Provider } from "react-redux";
+import store from "store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Rotas />
+      <Provider store={store}>
+        <Rotas />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
